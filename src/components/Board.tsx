fd3878da35b2ecd718d32d44bab0c9f08bb0728c
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-23 14:35:46 +0500

    added another setTimeout

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -110,16 +110,18 @@ const Board = ({ currentColor, currentSize, clearBoardKey }: BoardProp) => {
         socket.emit("clearCanvas");
       }
 
-      const canvas: HTMLCanvasElement | null = canvasRef.current;
-      const ctx = canvasRef.current?.getContext("2d");
+      const canvas = canvasRef.current;
+      const ctx = canvas?.getContext("2d");
 
       setTimeout(() => {
         ctx?.clearRect(0, 0, canvas!.width, canvas!.height);
-        const dataURL = canvas?.toDataURL();
 
+        const dataURL = canvas?.toDataURL();
         if (socket) {
-          socket.emit("canvasImage", dataURL);
-          console.log("Cleared board and emitted canvasImage");
+          setTimeout(() => {
+            socket.emit("canvasImage", dataURL);
+            console.log("Cleared board and emitted canvasImage");
+          }, 100);
         }
       }, 100);
     }
